b3bd3f73fa0a99b4ace84b82581f41d707dd730c
maxwelfrice <maxwelfrice06@gmail.com>
2022-05-13 17:40:12 +0700

    Update vis2 stacked bar chart with tooltip

--- COS30045 Project/scripts/stacked_bar_chart.js
+++ COS30045 Project/scripts/stacked_bar_chart.js
@@ -1,10 +1,18 @@
-var w = 500;
+var w = 600;
 var h = 500;
-var barPadding = 0.3;
-var chartPadding = 100;
+var barPadding = 0.3; //padding between bars
+var chartPadding = 100; //padding for the axis' space
+var svgRightPadding = 50;
+var tooltipYOffset = 200;
 
-//colors
-var color = d3.scaleOrdinal(d3.schemeCategory10);
+//colorscheme
+var color = d3.scaleQuantize()
+            .range(["#4535aa", "#b05cba", "#d6d1f5"]);
+
+// Define the div for the tooltip box
+var divTooltip = d3.select("body").append("div")	
+    .attr("class", "tooltip")				
+    .style("opacity", 0);
 
 //creating the stack visualization, with the .keys() specify which properties (series) in the dataset to use
 var stack = d3.stack()
@@ -14,7 +22,8 @@ var stack = d3.stack()
 var svg = d3.select("section")
             .append("svg")
             .attr("width", w)
-            .attr("height", h);
+            .attr("height", h)
+            .style("background", "white");
 
 
 function stackedBarChart(dataset){
@@ -31,16 +40,16 @@ function stackedBarChart(dataset){
     //a scale band is used for each stacked bar
     var xScale = d3.scaleBand()
                     .domain(dataset)
-                    .range([chartPadding, w])
+                    .range([chartPadding, w - svgRightPadding])
                     .paddingInner(barPadding);
 
     //scale linear for the values (the actual numbers)
     var yScale = d3.scaleLinear()
                     .domain([0, d3.max(dataset, function(d){
-                            return parseInt(d.production) + parseInt(d.consumption) + parseInt(d.netExport);
+                            return d.consumption + d.netExport;
                         })
                     ])
-                    .range([h - chartPadding, 0]);
+                    .range([h - chartPadding, chartPadding]);
 
     //drawing the stacked rectangles
     var rects = groups.selectAll("rect")
@@ -55,11 +64,47 @@ function stackedBarChart(dataset){
                     .attr("y", function(d){
                         return yScale(d[1]);
                     })
+                    .style("stroke", "black")
+                    .style("stroke-width", 1)
                     //bar width is the bandwidth of each band, minus padding (which is 0.1, previously stated)
                     .attr("width", xScale.bandwidth())
                     //height is the length from the first value to second value of the value pair
                     .attr("height", function(d){
                         return yScale(d[0]) - yScale(d[1]);
+                    }).on("mouseover", function(event, d) {
+
+                        //changing fill color
+                        // d3.select(this)
+                        //     .attr("fill", function(d, i){
+                        //         return color(i);
+                        //     });
+
+
+
+                        var pageX=event.pageX;
+                        var pageY=event.pageY;
+
+                        divTooltip.transition()
+                                    .duration(200)
+                                    .style("opacity", 0.9)
+                                    .style("width", xScale.bandwidth() + "px");
+                                    console.log(xScale.bandwidth());
+                        divTooltip.style.position = "absolute";
+                        divTooltip.html(d[1])
+                                    .style("left", d3.select(this).attr("x") + "px")
+                                    .style("top", (parseFloat(d3.select(this).attr("y")) + tooltipYOffset ) + "px");
+                    })
+                    .on("mouseout", function(d) {
+                        //removing the existing tooltip
+                        divTooltip.transition()		
+                            .duration(500)		
+                            .style("opacity", 0);	
+
+                        //changing fill color back
+                        // d3.select(this)
+                        //     .attr("fill", function(d, i){
+                        //         return color(i);
+                        //     });
                     });
 }
 
